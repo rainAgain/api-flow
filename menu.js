111ea9cd9970b851431da22ce4bdb0cb30b50ada
wckz <2582253358@qq.com>
2018-04-20 11:01:48 +0800

    add menu in mac

--- menu.js
+++ menu.js
@@ -130,6 +130,24 @@ var template = [{
         click: showWin
     }]
 }];
+
+//mac 环境需要添加下面的复制粘贴等代码
+if (process.platform === 'darwin') { 
+    template.unshift({
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' }
+        ]
+      })
+}
 module.exports = template;
 // var menu = Menu.buildFromTemplate(template);
 // Menu.setApplicationMenu(menu);
